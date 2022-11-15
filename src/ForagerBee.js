cee9ad42b25e7f114aba6f2b9f9bcf374fd10443
Wisesofthemall <foxxydieujuste@gmail.com>
2022-11-14 21:55:26 -0500

    Forager Bee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,4 +1,13 @@
 var ForagerBee = function() {
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 
 };
-
+ForagerBee.prototype.color = b.color;
+ForagerBee.prototype.food = g.food;
+ForagerBee.prototype.eat = g.eat;
+ForagerBee.prototype.forage = function(){
+  this.treasureChest.push('treasure')
+}
