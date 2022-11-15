90e05b5d1e8bf4aedbfd32561c676e9bde9af49b
Wisesofthemall <foxxydieujuste@gmail.com>
2022-11-14 22:00:13 -0500

    Complete Bare Minimum Requirement

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,18 @@
 var RetiredForagerBee = function() {
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
+var f =new ForagerBee();
+RetiredForagerBee.prototype.food = g.food;
+RetiredForagerBee.prototype.eat = g.eat;
+RetiredForagerBee.prototype.treasureChest = f.treasureChest;
 
+RetiredForagerBee.prototype.forage= function() {
+  return 'I am too old, let me play cards instead';
+}
+
+RetiredForagerBee.prototype.gamble = function(){
+  this.treasureChest.push('treasure');
+}
