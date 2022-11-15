5cdb8ad3b33ce9e6ca346155f77178175bdaecb4
Wisesofthemall <foxxydieujuste@gmail.com>
2022-11-14 21:51:36 -0500

    Complete HoneyMaker Bee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot= 0;
 };
 
+var b = new Bee();
+
+HoneyMakerBee.prototype.color = b.color;
+HoneyMakerBee.prototype.food = g.food;
+HoneyMakerBee.prototype.eat = g.eat;
+
+HoneyMakerBee.prototype.makeHoney = function(){
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
